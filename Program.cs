30d9f103f5fffbaa169b5fca4fac2d42c316f9fa
Gabriel Zanghelini <zanghelinigab@gmail.com>
2020-03-30 21:04:24 -0300

    commit inicial

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace problema_mochila_AG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
