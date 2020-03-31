49cf7091aaf1b0680cd58c2c211c53eafe47cccc
Jeferson Guesser <jefeguesser@gmail.com>
2020-03-30 22:01:13 -0300

    Added Item

---
+++ Item.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace problema_mochila_AG
+{
+    public class Item
+    {
+        private string Name {get;set;}
+        private string Peso {get;set;}
+        private string Pontos {get;set;}
+    }
+}
